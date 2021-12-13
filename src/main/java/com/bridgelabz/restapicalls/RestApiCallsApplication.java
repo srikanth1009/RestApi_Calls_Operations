6ca7a68b099b456ad0481a2e8faf801254cbc185
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-13 14:12:07 +0530

    Added: Creating A REST controller to respond various Http methods and Printing message

---
+++ src/main/java/com/bridgelabz/restapicalls/RestApiCallsApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restapicalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCallsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/restapicalls/RestApiCallsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restapicalls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCallsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
